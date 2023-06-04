bd865100f315bf49fa0583b7f91a53466c4c0f79
Tomasz Wasielewski <79154397+tom-wasi@users.noreply.github.com>
2023-06-04 16:50:35 +0200

    Add files via upload

---
+++ SocialMediaLinksEnvironmentsExample/SocialMediaLinksEnvironmentsExample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace SocialMediaLinksEnvironmentsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SocialMediaLinksOptions _options;
+
+        public HomeController(IOptions<SocialMediaLinksOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.SocialMediaLinks = _options;
+            return View();
+        }
+    }
+}
